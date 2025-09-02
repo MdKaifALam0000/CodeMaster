9baa569b138c8d880841c99b8bfa09749546c625
Md Kaif Alam <alam.kaif9430@gmail.com>
2025-09-03 00:20:27 +0530

    Updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,7 +16,7 @@ const videoRouter = require('./routes/videoCreator');
 //the browser protect the frontend to get connected with backedn because both are running at different port so to connect them we want a verfication that's why we use cors 
 //import cors -> and read the cors documentation
 app.use(cors({
-    origin:'https://codemaster-1.onrender.com',
+    origin:process.env.CLIENT_URL,
     credentials : true
 }))
 
